95a6b1df7b09039eec60320226493e76131bc334
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2026-01-13 18:03:54 +0530

    initial commit

---
+++ Two Pointers/validPalindrome.java
@@ -0,0 +1,24 @@
+public class validPalindrome {
+    public static boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println("String is Palindrome : " + isPalindrome(s));
+    }
+}
