7f039d4221776b75dffeda5fefabdef1e082fc05
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2026-01-16 19:38:54 +0530

    method 2 of solvinf trapping rainwater problem

--- Two Pointers/trapingRainwater24.java
+++ Two Pointers/trapingRainwater24.java
@@ -1,4 +1,5 @@
 public class trapingRainwater24{
+    //approach 1
     public static int trap(int[] height) {
         int left = 0;
         int right = height.length - 1;
@@ -26,8 +27,32 @@ public class trapingRainwater24{
         }
         return res;
     }
+
+    //approach 2
+     public static int trap2(int[] height) {
+        int n = height.length;
+        int left[] = new int[n];
+        left[0] = height[0];
+        for(int i=1;i<n;i++){
+            left[i] = Math.max(height[i],left[i-1]);
+        }
+
+        int right[] = new int[n];
+        right[n-1] = height[n-1];
+        for(int i=n-2;i>=0;i--){
+            right[i] = Math.max(height[i],right[i+1]);
+        }
+
+        int trappedWater = 0;
+        for(int i=0;i<n;i++){
+            int waterLevel=Math.min(left[i],right[i]);
+            trappedWater +=waterLevel-height[i];
+        }
+        return trappedWater;
+    }
     public static void main(String[] args) {
         int heights[] = {0,1,0,2,1,0,1,3,2,1,2,1};
         System.out.println(trap(heights));
+        System.out.println(trap2(heights));
     }
 }
