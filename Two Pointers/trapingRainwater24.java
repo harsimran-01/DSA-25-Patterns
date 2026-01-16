3a5a89d17d4c8550ac9c6e70694236e9bd36dba5
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2026-01-16 19:36:44 +0530

    traping rainwater leetcode 24

---
+++ Two Pointers/trapingRainwater24.java
@@ -0,0 +1,33 @@
+public class trapingRainwater24{
+    public static int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = Integer.MIN_VALUE;
+        int rightMax = Integer.MIN_VALUE;
+        int res = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] > leftMax) {
+                    leftMax = height[left];
+                } else {
+                    res += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] > rightMax) {
+                    rightMax = height[right];
+                } else {
+                    res += rightMax - height[right];
+                }
+                right--;
+            }
+
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int heights[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(heights));
+    }
+}
