258c658eb357a41a3307e64ae890f790896f1bb7
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2026-01-19 21:06:42 +0530

    move zeroes 283 leetcode

--- Two Pointers/moveZeroes283.java
+++ Two Pointers/moveZeroes283.java
@@ -16,6 +16,8 @@ public class moveZeroes283 {
     public static void main(String[] args) {
         int nums[] = {0,1,0,3,12};
         moveZero(nums);
-        
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i] + " ");
+        }
     }
 }
