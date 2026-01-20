ff1173240a456b64d335ae8a946232ef3a65c30f
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2026-01-20 19:44:32 +0530

    sqaures of sorted array leetcode 977

--- Two Pointers/squareofSortedArray977.java
+++ Two Pointers/squareofSortedArray977.java
@@ -1,3 +1,27 @@
+import java.util.*;
+
 public class squareofSortedArray977 {
-    
+    public static int[] sortedArray(int nums[]){
+        int n = nums.length;
+        int left = 0;
+        int right = n-1;
+        int res[] = new int[n];
+        int pos = n-1;
+
+        while(left <= right){
+            if(Math.abs(nums[left]) < Math.abs(nums[right])){
+                res[pos] = nums[right]*nums[right];
+                right--;
+            }else{
+                res[pos] = nums[left]*nums[left];
+                left++;
+            }
+            pos--;
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int nums[] = {-4,-1,0,3,10};
+        System.out.println(Arrays.toString(sortedArray(nums)));
+    }
 }
